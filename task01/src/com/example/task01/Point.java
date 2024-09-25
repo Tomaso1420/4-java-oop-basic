32c74cd646ab65d5ed0e6a15c91d4082615a34ca
Tomaso1420 <Artem82005@mail.ru>
2024-09-25 17:36:52 +0500

    Марциохо задание 1

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -7,6 +7,28 @@ public class Point {
     int x;
     int y;
 
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void flip() {
+        int temp = x;
+        x = -y;
+        y = -temp;
+    }
+
+    public double distance(Point point) {
+        double a = Math.pow(x - point.x, 2);
+        double b = Math.pow(y - point.y, 2);
+        return Math.sqrt(a + b);
+    }
+
+    public String toString() {
+        String output = Integer.toString(x) + ", " + Integer.toString(y);
+        return output;
+    }
+
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
